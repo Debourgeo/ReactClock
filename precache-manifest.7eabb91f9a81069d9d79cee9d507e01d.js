4ca5a06e8f99690cb9f79f1805e94e7e47e1d8e3
Your Name <you@example.com>
2020-02-12 15:51:52 +0100

    Updates

---
+++ precache-manifest.7eabb91f9a81069d9d79cee9d507e01d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4cfea3773aa1bc2b614b8762ee007e",
+    "url": "/ReactClock/index.html"
+  },
+  {
+    "revision": "3c48dca5e1608db617fb",
+    "url": "/ReactClock/static/css/main.2703f134.chunk.css"
+  },
+  {
+    "revision": "b4601aedd93b381b5f30",
+    "url": "/ReactClock/static/js/2.1e0ce2bf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactClock/static/js/2.1e0ce2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c48dca5e1608db617fb",
+    "url": "/ReactClock/static/js/main.7e9ed8c5.chunk.js"
+  },
+  {
+    "revision": "6644428caf6d0ba22345",
+    "url": "/ReactClock/static/js/runtime-main.aa4177e9.js"
+  }
+]);
